92a5805036874f01da17792e3cfdc8f4e80eb663
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-09 18:59:36 -0500

    Learned to create a simple hello world program

---
+++ java_4_Beginners_FCC/hello_World.java
@@ -0,0 +1,7 @@
+package java_4_Beginners_FCC;
+
+class hello_World { // In every java source file you can have one top level class and that class has to match the source file.
+    public static void main(String[] args) { // This line is the main method and serves as the entry point into your program
+        System.out.println("Hello world!"); // This method, prints out whatever string you pass through it. Also semi colons are required
+    }
+}

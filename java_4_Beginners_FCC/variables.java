a02e13e2302534a86367e17ce057038f6127ed56
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-09 19:13:43 -0500

    learned to initialize, assign, add, and print vars

---
+++ java_4_Beginners_FCC/variables.java
@@ -0,0 +1,15 @@
+package java_4_Beginners_FCC;
+
+class variables {
+    public static void main(String[] args){
+        int numberOne;
+        int numberTwo;
+        
+        numberOne = 3;
+        numberTwo = 9;
+
+        int addition = numberOne + numberTwo;
+    
+        System.out.println("Variable NumberOne: " + numberOne + " + Variable NumberTwo: " + numberTwo + " = " + addition);
+    }
+}

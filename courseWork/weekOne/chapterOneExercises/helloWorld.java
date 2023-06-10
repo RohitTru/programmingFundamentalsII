95c3c734fbeece64f38925647cb9723f79821748
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-10 01:21:49 -0500

    helloWorld exercise assignment

---
+++ courseWork/weekOne/chapterOneExercises/helloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * This program prints "Hello World" to the screen
+ * Written by Rohit Truesdale
+ * Written on 06/10/2023
+ * JDK version = 11.0.19
+ */
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World. "
+        + "\nRohit Truesdale");
+    }
+
+}

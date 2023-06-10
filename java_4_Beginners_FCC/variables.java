c76811586ffd68be94ed0ab7b64e934aa132a434
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-09 19:27:57 -0500

    learned about local variables vs global vars

--- java_4_Beginners_FCC/variables.java
+++ java_4_Beginners_FCC/variables.java
@@ -1,12 +1,14 @@
 package java_4_Beginners_FCC;
 
 class variables {
+    static int my_Global_Variable = 44; // Global variable b/c it is outside of the main method. FYI, required to be declared as a static.
     public static void main(String[] args){
-        int numberOne = 14;
-        int numberTwo = 9;
+        int localNumberOne = 14; // These are local variables since they are within the the main class. If we were to create another class, these variables would not be accessible.
+        int localNumbTwo = 9;
 
-        int addition = numberOne + numberTwo;
+        int addition = localNumberOne + localNumbTwo + my_Global_Variable;
     
-        System.out.println("Variable NumberOne: " + numberOne + " + Variable NumberTwo: " + numberTwo + " = " + addition);
+        System.out.println("Variable localNumberOne: " + localNumberOne + " + Variable localNumbTwo: " + localNumbTwo + 
+        " + my_Global_Variable: " + my_Global_Variable + " = " + addition);
     }
 }

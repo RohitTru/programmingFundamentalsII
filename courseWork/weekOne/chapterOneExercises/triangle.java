5ace7b3fb4c5fb69490897764423b27e440340ed
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-10 01:22:04 -0500

    triangle exercise assignment

---
+++ courseWork/weekOne/chapterOneExercises/triangle.java
@@ -0,0 +1,17 @@
+/*
+This program prints a triangle in the terminal
+Rohit Truesdale 06/10/2023
+JDK version = 11.0.1
+ */
+
+ public class triangle{
+    public static void main(String[] args){
+        System.out.println("                * "
+                       + "\n              * * * "
+                       + "\n            * * * * *"
+                       + "\n          * * * * * * *"
+                       + "\n        * * * * * * * * *"
+                       + "\n      * * * * * * * * * * *"
+                        );
+    }
+ }

9923ac544a710b4d30eacd66bbb2d62a555cd930
Rohit Truesdale <rohit.trues@gmail.com>
2023-07-09 21:29:49 -0500

    chapter7Assignment complete

---
+++ courseWork/week5/chapter7Assignment/chapter7Assignment.java
@@ -0,0 +1,63 @@
+package courseWork.week5.chapter7Assignment;
+
+import java.util.Scanner;
+
+public class chapter7Assignment {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numSodaTypes;
+        int totalBottlesSold = 0;
+        int highestBottlesSold = -1;
+        int lowestBottlesSold = Integer.MAX_VALUE;
+        int highestSoldIndex = -1;
+        int lowestSoldIndex = -1;
+        int[] numBottlesSold;
+        String[] sodaTypeNames;
+    
+        // Input the number of soda types from the user
+        do {
+            System.out.print("How many types of soda would you like to enter: ");
+            numSodaTypes = scanner.nextInt();
+        } while (numSodaTypes < 0);
+    
+        scanner.nextLine();
+    
+        // Input the names of the soda types in a String array
+        sodaTypeNames = new String[numSodaTypes];
+        for (int i = 0; i < numSodaTypes; i++) {
+            System.out.print("Enter the name of soda type " + (i + 1) + ": ");
+            sodaTypeNames[i] = scanner.nextLine();
+        }
+    
+        // Input the number of bottles sold for each soda type in an integer array
+        numBottlesSold = new int[numSodaTypes];
+        for (int i = 0; i < numSodaTypes; i++) {
+            do {
+                System.out.print("Enter the number of bottles sold for " + sodaTypeNames[i] + ": ");
+                numBottlesSold[i] = scanner.nextInt();
+            } while (numBottlesSold[i] < 0);
+    
+            totalBottlesSold += numBottlesSold[i];
+    
+            if (numBottlesSold[i] > highestBottlesSold) {
+                highestBottlesSold = numBottlesSold[i];
+                highestSoldIndex = i;
+            }
+    
+            if (numBottlesSold[i] < lowestBottlesSold) {
+                lowestBottlesSold = numBottlesSold[i];
+                lowestSoldIndex = i;
+            }
+        }
+    
+        System.out.println("---------------------------------------");
+        System.out.println("Total number of bottles sold: " + totalBottlesSold);
+        System.out.println("Highest selling soda: " + sodaTypeNames[highestSoldIndex]);
+        System.out.println("Lowest selling soda: " + sodaTypeNames[lowestSoldIndex]);
+    }
+    
+}
+
+
+

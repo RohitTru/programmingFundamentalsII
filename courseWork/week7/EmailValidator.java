bb405956833e056368b896c64bebe1aeee1e669d
Rohit Truesdale <rohit.trues@gmail.com>
2023-07-23 17:13:41 -0500

    assignment chapter 9 refactored

--- courseWork/week7/EmailValidator.java
+++ courseWork/week7/EmailValidator.java
@@ -4,44 +4,40 @@ import java.util.Scanner;
 
 public class EmailValidator {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String email;
 
-        // Ask user to enter email address
+        // Ask the user to enter their email address
         System.out.print("Enter your email address: ");
-        email = input.nextLine();
+        email = scanner.nextLine();
 
-        // Validate length of email address
+        // Validate the length of the email address
         if (email.length() < 15 || email.length() > 25) {
             System.out.println("Email address must be between 15-25 characters long.");
-        }
-        else {
-            // Validate @ symbol and number in email address
+        } else {
+            // Validate the presence of '@' symbol and a number in the email address
             boolean hasAtSymbol = false;
             boolean hasNumber = false;
 
             for (int i = 0; i < email.length(); i++) {
-                char c = email.charAt(i);
+                char character = email.charAt(i);
 
-                if (c == '@') {
+                if (character == '@') {
                     hasAtSymbol = true;
-                }
-                else if (Character.isDigit(c)) {
+                } else if (Character.isDigit(character)) {
                     hasNumber = true;
                 }
             }
 
             if (!hasAtSymbol) {
-                System.out.println("Email address must contain @ symbol.");
-            }
-            else if (!hasNumber) {
+                System.out.println("Email address must contain '@' symbol.");
+            } else if (!hasNumber) {
                 System.out.println("Email address must contain a number.");
-            }
-            else {
+            } else {
                 System.out.println("Email address is valid.");
             }
         }
 
-        input.close();
+        scanner.close();
     }
 }
